0a620422ea82951e7236af67592c57f75edd7e89
Chris Kalla <ChristopherKalla@MacBook-Pro.local>
2018-03-05 01:50:10 -0700

    Styling is complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,21 @@ $(document).ready(function() {
     //Load Song In Game//
     $("#SoLoud").get(0).play();
 
+    var myIndex = 0;
+    carousel();
+    
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+           x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
 
 
-
-
+    
 });
