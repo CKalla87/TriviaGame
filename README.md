28bc7f059f4eed13b1525dd7d337ed60ca56333e
Chris Kalla <ChristopherKalla@MacBook-Pro.local>
2018-03-12 00:13:23 -0700

    added final game logic

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TriviaGame
-Trivia Game
+# The Music Trivia Game
+<!------------------------------------------Welcome To My Music Trivia Game!---------------------------------------------------------------->
+
+Below are instructions on how to play the game:
+
+1. The game will start with the click of a button.
+
+2. You will have 15 seconds to answer each question.
+
+3. Each question is worth 1 point which will go towards your final score.
+
+4. If you do not answer a question in enough time, a new question will be loaded on the page and you will not get any points.
+
+5. If you miss a question you will be prompted on what the correct answer was.
+
+6. There are 15 total questions in this game and once you answer all the questions you will be taken to a page with your final scores.
+
+7. Once you get to the end of the game you are able to reset it at the click of a button.
+
+8. There are many links to other sites in the Header which will help you do better on answering the Trivia Questions.
+
+Good Luck and enjoy!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,22 +24,22 @@ $(document).ready(function() {
 
     //Setting 15 Questions To Game In An Array With Objects//
     var gameQuestions = [{
-        question: "What was the name of the band that changed the music scene in the early 90's from Hair Metal to Grundge?",
+        question: "What was the name of the band that changed the music scene in the early 90's from Hair Metal to Grunge?",
         answerList: ["Alice In Chains", "Pearl Jam", "Sound Garden", "Nirvana"],
         answer: 3
     
     }, {
         question: "What illness killed Freddy Mercury?",
         answerList: ["Cancer", "HIV", "AIDS", "Heart Attack"],
-        answer: 1
+        answer: 2
 
     }, {
         question: "Which inventor developed the solid-body electric guitar that made rock 'n roll possible??",
         answerList: ["Rupert Neve", "Les Paul", "Leo Fender", "C.F. Martin"],
-        answer: 0
+        answer: 1
     
     }, {
-        question: "Who Invented the most famouse recording console and music module in the world, (1073)?",
+        question: "Who Invented the most famous recording console and music module in the world, (1073)?",
         answerList: ["Bill Putnam", "Rupert Neve", "Georg Neumann", "Sherman Fairchild"],
         answer: 1
     
@@ -51,7 +51,7 @@ $(document).ready(function() {
     }, {
         question: "What famous female singer died of alcohol poisoning in 2011 at the age of 27?",
         answerList: ["Mia Zapata", "Amy Winehouse", "Mindy McCready", "Chrissy Amplhett"],
-        answer: 3
+        answer: 1
 
     }, {
         question: "What was the highest selling album of the 1980's?",
@@ -59,13 +59,13 @@ $(document).ready(function() {
         answer: 2
 
     }, {
-        question: "Which Rap Rock Band would sing about Politics and Current Issues?",
+        question: "Which rap-rock band would sing about politics and current issues?",
         answerList: ["The Rolling Stones", "The Beatles", "Audio Slave", "Rage Against The Machine"],
         answer: 3
 
     }, {
-        question: "At what unusual institutions did Johnny Cash record a number of live albums?",
-        answerList: ["Prisons", "His House", "Sun Record Company", "On The Road"],
+        question: "At what unusual institution did Johnny Cash record a number of live albums?",
+        answerList: ["Prison", "His House", "Sun Record Company", "On The Road"],
         answer: 0
 
     }, {
@@ -94,11 +94,195 @@ $(document).ready(function() {
         answer: 2
 
     }, {
-        question: "What famouse Classical Artist was both deaf and going mad from Lead Poisoning?",
+        question: "What famous classical artist was both deaf and going mad from lead poisoning?",
+        answerList: ["Wolfgang Amadeus Mozart", "Fredric Chopin", "Claude Debussy", "Ludwig van Beethoven"],
+        answer: 3
+    }, {
+        question: "What famous classical artist was both deaf and going mad from lead poisoning?",
         answerList: ["Wolfgang Amadeus Mozart", "Fredric Chopin", "Claude Debussy", "Ludwig van Beethoven"],
         answer: 3
     }];
-
-
     
+    //Answers Variable Array
+    var answerKey = ["Nirvana", "AIDS", "Les Paul", "Rupert Neve", "Leonard Cohen", "Amy Winehouse", "Thriller", "Rage Against The Machine", "Prisons", "Jimmy Marshall Hendrix", "Woodstock", "Candle in the Wind (1997)", "Video Killed the Radio Star", "Elton John", "Ludwig van Beethoven"];
+    var index = 0;
+    var number = 15;
+    var intervalId;
+    var correctCounter = 0;
+    var incorrectCounter = 0;
+    var current = 0;
+    var ButtonA = 0;
+    var ButtonB = 1;
+    var ButtonC = 2;
+    var ButtonD = 3;
+    var key = 0;
+    
+    
+    //Timer code
+    //Timer start function
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    };
+
+    //Decrement function   
+    function decrement() {
+        number--;
+        $("#timer").html("<h2>" + number + " Seconds Remaining!" + "</h2>");
+        if (number === 0) {
+            stop();
+            alert("I guess you are not a  music master!")
+            current++
+            loadQuestion();
+            reset();
+        }
+    };
+
+    //Reset function
+    function reset() {
+        number = 15;
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+        $("#timer").html("<h2>" + number + " Seconds Remaining!" + "</h2>");
+    }
+
+   
+    //Stop function
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+ 
+    //First Button Push To Start Game
+    function buttonReset() {
+    stop();
+    $("#timer").html("<h2>" + number + " Seconds Remaining!" + "</h2>").empty("#timer");
+    $("#question").text(gameQuestions[0].question).empty("question");
+    $("#buttonA").text(gameQuestions[0].answerList[0]).hide("#buttonA");
+    $("#buttonB").text(gameQuestions[0].answerList[1]).hide("#buttonB");
+    $("#buttonC").text(gameQuestions[0].answerList[2]).hide("#buttonC");
+    $("#buttonD").text(gameQuestions[0].answerList[3]).hide("#buttonD");
+    };
+    
+    //Game Start
+    function start() {
+        buttonReset ()
+        $("#welcome").html("<h3>" + " Welcome to the Music Trivia Game, let's see if you know your stuff! " + "</h3>");
+        $("#start").html("Click The Button To Start The Game! ");
+        $("#start").on("click", function() {
+            $(this).hide();
+            $("#welcome").hide();
+            loadQuestion();
+           });
+        }
+    //Run Function
+    start();
+
+   
+    //Final Score function
+    function scoreBoard() {
+    if (current === 15) {
+        buttonReset ()
+        correctCounter;
+        $("#correct").html("Answered Correctly: " + correctCounter);
+        incorrectCounter;
+        $("#incorrect").html("Answered Incorrectly: " + incorrectCounter);
+        //Restart the game
+        $("#start").html("Click This Button To Re-Start The Game! ").show();
+        $("#start").on("click", function() {
+            $(this).hide();
+            $("#correct").hide();
+            $("#incorrect").hide();
+            location.reload();
+        });
+    } else {
+
+    }
+    };
+    
+    //Questons To Load From Object
+    function loadQuestion() {
+    run();
+    reset();
+    $("#question").text(gameQuestions[current].question);
+    $("#buttonA").text(gameQuestions[current].answerList[0]).show();
+    $("#buttonB").text(gameQuestions[current].answerList[1]).show();
+    $("#buttonC").text(gameQuestions[current].answerList[2]).show();
+    $("#buttonD").text(gameQuestions[current].answerList[3]).show();
+    };
+
+    //Click function for buttons
+    $("#buttonA").on('click', function() {
+    if(ButtonA === gameQuestions[current].answer) {
+        alert("Look At You Go!")
+        correctCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+        
+    } else {
+        alert("Sorry " + answerKey[key] + " is the answer.");
+        incorrectCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+    }
+    })
+    
+    $("#buttonB").on('click', function() {
+    if(ButtonB === gameQuestions[current].answer) {
+        alert("Look At You Go!")
+        correctCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+    } else {
+        alert("Sorry " + answerKey[key] + " is the answer.");
+        incorrectCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+        }
+    })
+
+
+    $("#buttonC").on('click', function() {
+    if(ButtonC === gameQuestions[current].answer) {
+        alert("Look At You Go!")
+        correctCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+    } else {
+        alert("Sorry " + answerKey[key] + " is the answer.");
+        incorrectCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+        }
+    })
+
+    $("#buttonD").on('click', function() {
+    if(ButtonD === gameQuestions[current].answer) {
+        alert("Look At You Go!")
+        correctCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+    } else {
+        alert("Sorry " + answerKey[key] + " is the answer.");
+        incorrectCounter++
+        current++
+        key++
+        loadQuestion();
+        scoreBoard();
+        }
+    });
 });
